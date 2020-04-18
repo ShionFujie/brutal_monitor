9ace0b346e6146243c7c7505b58a5dfd3fdd8160
shion.t.fujie <tonaluna32966@gmail.com>
2020-04-18 13:45:32 +0900

    fromPortMessage.js

--- js/background.js
+++ js/background.js
@@ -2,18 +2,6 @@ const Rx = rxjs;
 
 fromPortMessages(PORT_NAME_USER_ACTIVITY).subscribe(onDetectUserActivity);
 
-function fromPortMessages(portName) {
-  return Rx.fromEventPattern(
-    handler => {
-      chrome.runtime.onConnect.addListener(({ name, onMessage }) => {
-        if (name == portName) onMessage.addListener(handler);
-      });
-    },
-    undefined,
-    (message, _) => message
-  );
-}
-
 function onDetectUserActivity({ datetime, type, url }) {
   console.log(`${datetime.toString()}:[${type}] ${url}`);
 }
