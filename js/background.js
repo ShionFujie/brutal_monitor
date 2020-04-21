a55c948bce9c36c1ca513a3f112e95d47ec6648b
shion.t.fujie <tonaluna32966@gmail.com>
2020-04-21 21:40:18 +0900

    sampling per minute

--- js/background.js
+++ js/background.js
@@ -1,4 +1,8 @@
-fromPortMessages(PORT_NAME_USER_ACTIVITY).subscribe(onDetectUserActivity);
+const sampleTime = rxjs.operators.sampleTime;
+
+fromPortMessages(PORT_NAME_USER_ACTIVITY)
+  .pipe(sampleTime(60000))
+  .subscribe(onDetectUserActivity);
 
 function onDetectUserActivity({ datetime, type, url }) {
   console.log(`${datetime.toString()}:[${type}] ${url}`);
