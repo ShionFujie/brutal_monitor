0624a28b7f718dfbd509326c6779120ad625ef84
shion.t.fujie <tonaluna32966@gmail.com>
2020-04-22 15:13:19 +0900

    promise to observable

--- js/stores/UserActivityStore.js
+++ js/stores/UserActivityStore.js
@@ -2,11 +2,13 @@ const UserActivityStore = firebase => {
   const db = firebase.firestore();
 
   function addActivity({ datetime, url }) {
-    db.collection("userActivity")
+    const prms = db
+      .collection("userActivity")
       .add({ datetime, url })
       .then(async () => ({ complete: true }))
       .catch(async reason => ({ complete: false, reason }));
+    return rxjs.from(prms);
   }
 
-  return {addActivity}
+  return { addActivity };
 };
